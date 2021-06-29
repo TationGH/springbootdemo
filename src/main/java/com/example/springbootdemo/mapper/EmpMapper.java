fb0223047f28338b6848c62a431c28fe6ffdc6b6
tation <2012592020@qq.com>
2021-06-30 06:50:23 +0800

    Revert "Initial commit"
    This reverts commit dcc00cff

--- src/main/java/com/example/springbootdemo/mapper/EmpMapper.java
+++ src/main/java/com/example/springbootdemo/mapper/EmpMapper.java
@@ -1,12 +1,10 @@
 package com.example.springbootdemo.mapper;
 
 import com.example.springbootdemo.entity.EmpEntity;
-import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
-import java.util.Map;
 
-@Mapper
+//@Mapper
 public interface EmpMapper {
     List<EmpEntity> getEnameById(String id);
 }
